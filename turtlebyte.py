0facfca42c43433583432392eab6796bfc171800
isaac1000000 <isaac.fisher@nyu.edu>
2025-06-15 15:58:32 -0400

    Can read bytes

--- turtlebyte.py
+++ turtlebyte.py
@@ -11,8 +11,13 @@ author: isaac1000000
 import os
 import turtle as t
 from dotenv import load_dotenv
+from utils import normalize, detection
 
 load_dotenv('.env', override=True)
+
+# HIGHLY experimental. Don't change unless you've got some big ideas...
+BLOCK_SIZE = 512
+BYTE_ORDER = 'little'
 
 try:
     TURTLE_PEN_SIZE = int(os.getenv('TURTLE_PEN_SIZE'))
@@ -20,23 +25,57 @@ try:
     TURTLE_SCREENSIZE_X = int(os.getenv('TURTLE_SCREENSIZE_X'))
     TURTLE_SCREENSIZE_Y = int(os.getenv('TURTLE_SCREENSIZE_Y'))
     TURTLE_WINDOW_BUFFER = int(os.getenv('TURTLE_WINDOW_BUFFER'))
+
     turtle_origin = ((-TURTLE_SCREENSIZE_X+TURTLE_PEN_SIZE)//2+TURTLE_WINDOW_BUFFER, (TURTLE_SCREENSIZE_Y-TURTLE_PEN_SIZE)//2-TURTLE_WINDOW_BUFFER)
 except Exception:
     print("Error loading environment variables. Ensure your .env file is properly configured")
     exit()
 
+normalizer = normalize.Normalizer(turtle_origin, TURTLE_PEN_SIZE, BLOCK_SIZE)
+detector = detection.Detector(t)
 
-def initialize():
+def read_byte(address:bytes) -> bytes:
+    """
+    Reads a byte at a specific address in memory.
 
-    screen = t.Screen()
-    screen.setup(width=TURTLE_SCREENSIZE_X, height=TURTLE_SCREENSIZE_Y)
+    Args:
+        address (int): the address to read.
+    Returns:
+        bytes: the byte found at address.
+    """
+    t.setposition(turtle_origin)
+    t.seth(0)
 
-    t.pensize(TURTLE_PEN_SIZE)
-    t.speed(TURTLE_SPEED)
-    t.pu()
-    t.setposition(*turtle_origin)
+    byte = _read_nibble()
+    byte = byte << 4
+    _r()
+    _f()
+    _r()
+    byte += _read_nibble()
 
-def write_byte(address: int, data: bytes) -> bool:
+    return byte.to_bytes(1, BYTE_ORDER)
+
+
+def _read_nibble() -> int:
+    """
+    An internal function that returns an int from the current nibble
+
+    Returns:
+        int: the int found at the current nibble
+    """
+
+    result = 0
+    for i in range(3):
+        result = result << 1
+        if detector.marked():
+            result += 1
+        _f()
+    result = result << 1
+    if detector.marked():
+        result += 1
+    return result
+
+def write_byte(address: bytes, data: bytes) -> bool:
     """
     Writes a byte to a specific address in memory.
 
@@ -52,17 +91,25 @@ def write_byte(address: int, data: bytes) -> bool:
 
     #TODO: move to memory address
 
-    bit_array = [int.from_bytes(data, 'little') & 2**i != 0 for i in range(7, -1, -1)] # endian-ness does not matter here b/c one byte
+    bit_array = [int.from_bytes(data, BYTE_ORDER) & 2**i != 0 for i in range(7, -1, -1)] # endian-ness does not matter here b/c one byte
     print(bit_array)
 
-    _write_nibble_at_current(bit_array[:4])
+    try:
+        _write_nibble_at_current(bit_array[:4])
+    except Exception:
+        return False
     
     # Move down a row to continue writing the byte
     _r()
     _f()
     _r()
 
-    _write_nibble_at_current(bit_array[4:])
+    try:
+        _write_nibble_at_current(bit_array[4:])
+    except Exception:
+        return False
+
+    return True
 
 
 def _write_nibble_at_current(nib: list[bool]) -> None:
@@ -70,7 +117,7 @@ def _write_nibble_at_current(nib: list[bool]) -> None:
     An internal function for writing 4 bits' worth of information
 
     Args:
-        nib(list[bool]): A length-4 list of boolean values
+        nib (list[bool]): A length-4 list of boolean values
     """
 
     assert len(nib) == 4, "Invalid operation: attempt to write nibble of improper length"
@@ -126,11 +173,20 @@ def _m() -> None:
     """
     t.dot(TURTLE_PEN_SIZE)
 
+def initialize():
+
+    screen = t.Screen()
+    screen.setup(width=TURTLE_SCREENSIZE_X, height=TURTLE_SCREENSIZE_Y)
+
+    t.pensize(TURTLE_PEN_SIZE)
+    t.speed(TURTLE_SPEED)
+    t.pu()
+    t.setposition(*turtle_origin)
 
 if __name__ == "__main__":
     initialize()
     write_byte(0, b'\xAA')
-    t.hideturtle()
-
+    t.setposition(turtle_origin)
+    print(read_byte(0))
 
     input()
